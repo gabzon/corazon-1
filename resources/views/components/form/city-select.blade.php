5f7c38009718be15617f31d7858c4eb8d040df05
Gabriel Zambrano <gab.zambrano@gmail.com>
2021-07-16 10:02:53 +0200

    select city fixed

--- resources/views/components/form/city-select.blade.php
+++ resources/views/components/form/city-select.blade.php
@@ -3,7 +3,7 @@
     <select id="city" name="city" {{ $attributes }}
         class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md @error($name) border-red-600 @enderror">
         <option value="" default selected>Choose city</option>
-        @foreach (\App\Models\city::all() as $city)
+        @foreach (\App\Models\City::all() as $city)
         <option value="{{ $city->id }}">{{ $city->name }}</option>
         @endforeach
     </select>
