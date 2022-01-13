d0933a0741f3383c64d723a1586eee99236dad95
Gabriel Zambrano <gab.zambrano@gmail.com>
2022-01-13 09:55:43 +0100

    button updated 7

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -6,10 +6,10 @@
             </div>
             @auth
             <div class="flex md:mt-0 md:ml-4">
-                <x-ui.button route="{{ url()->previous() }}" color="secondary">
+                <a href="{{ url()->previous() }}"
+                    class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Back
-                </x-ui.button>
-
+                </a>
                 @can('update', $event)
                 <a href="{{ route('event.edit', $event) }}"
                     class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -36,5 +36,4 @@
             <div class="my-16">&nbsp;</div>
         </div>
     </div>
-
 </x-app-layout>
