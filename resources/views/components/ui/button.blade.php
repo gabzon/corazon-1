133264217693abc5571019f4e6c6c7450ac79a61
Gabriel Zambrano <gab.zambrano@gmail.com>
2022-01-13 09:47:30 +0100

    button updated 3

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ $route }}"
-    class="inline-flex items-center border font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 {{ $color }} {{ $size ?? 'md' }} {{ $shape }} {{ $css }}">
+    class="inline-flex items-center border font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 {{ $color }} {{ $size ?? 'md' }} {{ $shape ?? 'rounded-md' }} {{ $css }}">
     {{ $slot }}
 </a>
