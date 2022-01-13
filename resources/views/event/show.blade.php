eb88b3b4c1e14e4b62f8734ccdb0b964c7776d37
Gabriel Zambrano <gab.zambrano@gmail.com>
2022-01-13 09:40:20 +0100

    event show designed fixed

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -1,20 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="md:flex md:items-center md:justify-between">
+        <div class="flex items-center justify-between">
             <div class="flex-1 min-w-0">
                 <x-typo.page-heading title="{{ $event->name }}" />
             </div>
             @auth
-            <div class="mt-4 flex md:mt-0 md:ml-4">
-                <a href="{{ url()->previous() }}"
-                    class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <div class="flex md:mt-0 md:ml-4">
+                <x-ui.button route="{{ url()->previous() }}" color="secondary">
                     Back
-                </a>
+                </x-ui.button>
+
                 @can('update', $event)
-                <a href="{{ route('event.edit', $event) }}"
-                    class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <x-ui.button route="{{ route('event.edit', $event) }}" css="ml-3">
                     Edit
-                </a>
+                </x-ui.button>
                 @endcan
 
             </div>
@@ -22,8 +21,8 @@
         </div>
     </x-slot>
 
-    <div class="bg-gray-100">
-        <div class="max-w-2xl mx-auto py-10 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div class="bg-gray-100 h-screen overflow-y-scroll">
+        <div class="max-w-2xl mx-auto py-4 sm:py-6 md:py-8 lg:py-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
             <div class="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
                 <div class="flex flex-col">
                     @include('event.show.left')
@@ -33,7 +32,7 @@
                     @include('event.show.right')
                 </div>
             </div>
-            <div class="my-8">&nbsp;</div>
+            <div class="my-16">&nbsp;</div>
         </div>
     </div>
 
